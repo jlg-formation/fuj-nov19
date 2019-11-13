9154daa98db345d1bc10c593fd42f0383f2bd3a2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-13 10:04:05 +0100

    added routes form and success

--- front/src/app/create-ref/create-ref-routing.module.ts
+++ front/src/app/create-ref/create-ref-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { SuccessComponent } from './success/success.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'form', component: FormComponent },
+  { path: 'success', component: SuccessComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CreateRefRoutingModule { }
+export class CreateRefRoutingModule {}
--- front/src/app/create-ref/create-ref.module.ts
+++ front/src/app/create-ref/create-ref.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CreateRefRoutingModule } from './create-ref-routing.module';
+import { FormComponent } from './form/form.component';
+import { SuccessComponent } from './success/success.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FormComponent, SuccessComponent],
   imports: [
     CommonModule,
     CreateRefRoutingModule
