819f46da150a66e46470dc5868a64fa9df7aac92
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-14 10:26:38 +0100

    added routes

--- front/src/app/sell-ref/sell-ref-routing.module.ts
+++ front/src/app/sell-ref/sell-ref-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { SuccessComponent } from './success/success.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sell/list', component: ListComponent },
+  { path: 'sell/detail', component: DetailComponent },
+  { path: 'sell/success', component: SuccessComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class SellRefRoutingModule { }
+export class SellRefRoutingModule {}
