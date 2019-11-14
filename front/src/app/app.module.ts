0b25c83bbbbd2a3132130e4f05ff7e472bf4326c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-14 10:21:13 +0100

    create module sell and routes

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -10,10 +10,11 @@ import { CreateRefModule } from './create-ref/create-ref.module';
 import { ReferenceService } from './service/reference.service';
 import { HttpReferenceService } from './service/http-reference.service';
 import { HttpClientModule } from '@angular/common/http';
+import { SellRefModule } from './sell-ref/sell-ref.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule, CreateRefModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, CreateRefModule, HttpClientModule, SellRefModule],
   providers: [{ provide: ReferenceService, useClass: HttpReferenceService }],
   bootstrap: [AppComponent]
 })
