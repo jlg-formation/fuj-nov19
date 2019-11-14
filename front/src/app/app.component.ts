cb955f2ef78f166589f65af69f5220b65c3d8a4c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-14 14:23:10 +0100

    ok

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReferenceService } from './service/reference.service';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front';
-
-  toto() {
-    console.log('toto');
-  }
+  constructor(private reference: ReferenceService) {}
 }
--- front/src/app/service/http-reference.service.ts
+++ front/src/app/service/http-reference.service.ts
@@ -10,6 +10,7 @@ export class HttpReferenceService extends ReferenceService {
   constructor(private http: HttpClient) {
     super();
     console.log('http service');
+    this.fetch();
   }
 
   add(reference: Reference) {
@@ -22,7 +23,24 @@ export class HttpReferenceService extends ReferenceService {
         console.error('err: ', err);
       },
       complete: () => {
-        console.log('complete');
+        console.log('complete post');
+      }
+    });
+  }
+
+  fetch() {
+    console.log('fetch');
+    this.http.get<Reference[]>('http://localhost:3000/ws/reference').subscribe({
+      next: references => {
+        console.log('references: ', references);
+        this.refSet = new Set<Reference>(references);
+        this.saveRefSet();
+      },
+      error: err => {
+        console.error('err: ', err);
+      },
+      complete: () => {
+        console.log('complete get');
       }
     });
   }
